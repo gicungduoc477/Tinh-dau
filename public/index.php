06b0e59622e2ce653ebb3755b374bc622e0732d8
Bui Van Hieu <email_github_vanhieubui403@gmail.com>
2026-03-19 02:05:15 +0700

    fix lỗi khiếu nại

--- public/index.php
+++ public/index.php
@@ -4,14 +4,16 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Kiểm tra xem ứng dụng có đang ở chế độ bảo trì không...
+// 1. Kiểm tra chế độ bảo trì
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-// Đăng ký bộ tải tự động của Composer...
+// 2. Đăng ký Composer Autoloader
 require __DIR__.'/../vendor/autoload.php';
 
-// Khởi tạo Laravel và xử lý yêu cầu...
-(require_once __DIR__.'/../bootstrap/app.php')
-    ->handleRequest(Request::capture());
+// 3. Khởi tạo Laravel và xử lý Request
+// Đảm bảo đường dẫn tới bootstrap/app.php chính xác
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+$app->handleRequest(Request::capture());
